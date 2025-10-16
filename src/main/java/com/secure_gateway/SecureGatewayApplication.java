7f24443976c00be1d685f1d50acb961247747ebe
Jenson <kokjenson1234@gmail.com>
2025-10-17 02:58:06 +0800

    second commit

---
+++ src/main/java/com/secure_gateway/SecureGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.secure_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure_gateway/SecureGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
